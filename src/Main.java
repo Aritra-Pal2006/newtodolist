d916b5abfdd5ad35741decdadfb29b14ea7a7f43
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-02-13 20:29:19 +0530

    code for todo is added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class test
+{
+    ArrayList<String> list = new ArrayList<String>();
+    void intolist(String todo)
+    {
+
+        list.add(todo);
+
+    }
+    void display()
+    {
+        for (String s : list)
+        {
+            System.out.println(s);
         }
+    }
+
+    void delete()
+    {
+        Scanner sc=new Scanner(System.in);
+        String del=sc.nextLine();
+        list.remove(del);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        test obj=new test();
+
+        while(true)
+        {
+            System.out.println("Press 1 for taking the inputs from the user");
+            System.out.println("Press 2 for displaying");
+            System.out.println("Press 3 for deletion");
+
+            int choice = sc.nextInt();
+
+
+            switch(choice) {
+
+                case 1 :
+                    obj.input();
+                    break;
+
+                case 2 :
+                    obj.display();
+                    break;
+
+                case 3 :
+                    obj.delete();
+                    break;
+
+
+            }
+
+        }
+
+
+
+    }
+    public void input()
+    {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Todo");
+
+        String ns=sc.nextLine();
+        intolist(ns);
+
+
+
     }
 }
